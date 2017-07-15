dda46a4e2958d7c2ac20cfeb756f6d3e264e487b
xqj <xqj198404@gmail.com>
2017-07-16 01:57:38 +0800

    1

---
+++ DatabaseCodeTool/Tool/Model/Database.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool.Model
+{
+    [Serializable]
+    class Database
+    {
+        public string name { set; get; }
+        public string type { set; get; }
+        public string value { set; get; }
+    }
+}
